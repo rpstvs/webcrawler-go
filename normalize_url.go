d447ca77f50efb9bfca38022712a7d1985571ae3
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-09-20 12:05:52 +0100

    added implementation of normalize url

--- normalize_url.go
+++ normalize_url.go
@@ -1,1 +1,13 @@
 package main
+
+import "net/url"
+
+func normalizeURL(urL string) (string, error) {
+
+	parsedURL, err := url.Parse(urL)
+
+	if err != nil {
+		return "", err
+	}
+	return parsedURL.Host + parsedURL.Path, nil
+}
