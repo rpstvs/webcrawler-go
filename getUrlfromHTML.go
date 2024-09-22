28a7afb16077a2b3b5c4191f520ede9c180fed39
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-09-22 10:27:44 +0100

    continued with implementation of new function

--- getUrlfromHTML.go
+++ getUrlfromHTML.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
 func getUrlFromHtml(htmlBody, rawBaseURL string) ([]string, error) {
+	reader := strings.NewReader(htmlBody)
+
+	tree, err := html.Parse(reader)
+	if err != nil {
+		log.Println("Couldnt read html body")
+	}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		fmt.Println(n.Data)
+		if n.Type == html.ElementNode && n.Data == "a" {
+
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(tree)
 
 	return []string{""}, nil
 }
