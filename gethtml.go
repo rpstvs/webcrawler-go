a7f950bb4392996dbc6c67f5f5b5280099278211
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-09-29 15:27:13 +0100

    added function to get html from url

---
+++ gethtml.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func getHtmml(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+
+	if err != nil {
+		return "", fmt.Errorf("error on http")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 {
+		fmt.Println("Error on response")
+		return "", fmt.Errorf("Error on response")
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+
+	if !strings.Contains(contentType, "text/html") {
+		return "", fmt.Errorf("error content type")
+	}
+
+	dat, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", fmt.Errorf("Error on reading body")
+	}
+
+	c := string(dat)
+
+	fmt.Println(c)
+
+	return c, nil
+}
--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("no website provided")
+
+		fmt.Println("no website provided")
+		os.Exit(1)
 	} else if len(os.Args) > 2 {
-		log.Fatal("too many arguments")
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
 	} else {
 		baseURL := os.Args[1]
-		fmt.Printf("Starting webcrawler on: %s \n", baseURL)
+		c, _ := getHtmml(baseURL)
+		fmt.Printf("starting crawl: %s \n", baseURL)
+		fmt.Println(c)
 	}
 
 }
